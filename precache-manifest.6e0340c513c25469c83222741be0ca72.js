ce81066d27cbf10258ec4efcc86117cf335e459e
Juan Betancur <pablo@Juans-MBP.lan>
2021-03-02 20:04:23 -0500

    Updates

---
+++ precache-manifest.6e0340c513c25469c83222741be0ca72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d04a8b11a345703c07ab83f67ec2da",
+    "url": "/React-Todo-app-master/index.html"
+  },
+  {
+    "revision": "368e942422927bdc8c3d",
+    "url": "/React-Todo-app-master/static/css/main.40a96142.chunk.css"
+  },
+  {
+    "revision": "b3e766b1119338dc48a4",
+    "url": "/React-Todo-app-master/static/js/2.ddb3d87b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/React-Todo-app-master/static/js/2.ddb3d87b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368e942422927bdc8c3d",
+    "url": "/React-Todo-app-master/static/js/main.dac09bc2.chunk.js"
+  },
+  {
+    "revision": "f4226f25e5a4139fac26",
+    "url": "/React-Todo-app-master/static/js/runtime-main.908d292d.js"
+  },
+  {
+    "revision": "4f646371b2a2501c7acd9110625993ca",
+    "url": "/React-Todo-app-master/static/media/background.4f646371.jpg"
+  }
+]);
